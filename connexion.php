9e3825463eae616a53173b786a15a21e77bc0a5c
wiss-ali <wiss.dev@outlook.fr>
2023-12-07 21:44:51 +0100

    page formulaire d'inscription

---
+++ connexion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+
+<form action="traitement.php" method="post">
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" name="nom" required>
+    <br>
+
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" required>
+    <br>
+
+    <label for="pseudo">Pseudo :</label>
+    <input type="text" id="pseudo" name="pseudo" required>
+    <br>
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="mdp">Mot de passe :</label>
+    <input type="password" id="mdp" name="mdp" required>
+    <br>
+    <button type="submit">S'inscrire</button>
+</form>
+
+</body>
+</html>
