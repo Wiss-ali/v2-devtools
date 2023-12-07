3b8414afdbd9b3b877d0035cb8af0edcb14c733c
wiss-ali <wiss.dev@outlook.fr>
2023-12-07 22:33:49 +0100

    ajout h2 et h3

--- connexion.php
+++ connexion.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 <h1>salut je teste un formulaire ici</h1>
+<h2>wsh celea bien ou quoi</h2>
+<h3>c'est une page de merde la on s'en tape mais c'est en ligne</h3>
 <form action="traitement.php" method="post">
     <label for="nom">Nom :</label>
     <input type="text" id="nom" name="nom" required>
