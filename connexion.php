a43d1d8ccd0fd02952c2cba476879b3a890aa47e
wiss-ali <wiss.dev@outlook.fr>
2023-12-12 04:46:10 +0100

    ajout de page de connexion

---
+++ connexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+</head>
+<body>
+    
+<form action="traitement_connexion.php" method="post">
+    <label for="pseudo">Pseudo :</label>
+    <input type="text" id="pseudo" name="pseudo" required>
+
+    <label for="mdp">Mot de passe :</label>
+    <input type="password" id="mdp" name="mdp" required>
+
+    <imput type="submit" name="btn-conn">Se Connecter</button>
+</form>
+    
+</body>
+</html>
